cf26406ad8efedbf41188ddeb4f52e1970676129
Compilenix <compilenix@gmail.com>
2018-10-13 01:21:45 +0200

    improve i3 lock script performance
    using jpg as long as possible, because its better for this kind of images
    and scrot creates the screenshot faster for jpg than png.
    i3lock only works with png's

--- home/bin_dotfiles/lock.sh
+++ home/bin_dotfiles/lock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 2.5% -scale 4000% /tmp/screen.png
-# convert /tmp/screen.png -scale 1.25% -scale 8000% /tmp/screen.png
+scrot -q 100 /tmp/screen.jpg
+convert /tmp/screen.jpg -scale 2.5% -scale 4000% /tmp/screen.png
+# convert /tmp/screen.jpg -scale 1.25% -scale 8000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop 2>/dev/null
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop 2>/dev/null &
 i3lock -i /tmp/screen.png
+rm /tmp/screen.jpg
 rm /tmp/screen.png

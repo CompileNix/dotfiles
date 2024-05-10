a46c50e6ea77ade2caa74259976c1f1f2835ab44
CompileNix <compilenix@gmail.com>
2024-05-10 15:53:47 +0200

    update wl-xwayland-get-active-output: fix regex

--- home/dotfiles_bin/wl-xwayland-get-active-output.sh
+++ home/dotfiles_bin/wl-xwayland-get-active-output.sh
@@ -4,6 +4,6 @@
 # Second variant ist much faster
 # RE="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused).rect | "\(.width)\\/\\d+?x\(.height)\\/\\d+?\\+\(.x)\\+\(.y)"')"
 RE="$(swaymsg -t get_outputs | jtc -w'[:][focused]<true>b[-1][rect]' -qqT'"{$b}\\\/\\d+?x{$a}\\\/\\d+?\\+{$c}\\+{$d}"')"
-OUTPUT="$(xrandr --listmonitors | grep -P $RE | grep -Po '\d+?: \+\K(\w+)')"
+OUTPUT="$(xrandr --listmonitors | grep -P $RE | grep -Po '\d+?: \+\K(\w+-?\d{0,2})')"
 echo $OUTPUT
 
